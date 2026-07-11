917887a16f74509dfd2ee9d1202da942570aef3b
proudbhebhe <proudbhebhe02@gmail.com>
2026-07-11 20:16:36 +0200

    The hamburger menu is finally functional

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,10 +2,13 @@ const today = new Date();
 const currentYear = today.getFullYear();
 const hamButton = document.querySelector('#menu');
 const nav = document.querySelector('nav');
+const para = document.querySelector('p');
 
 document.getElementById("currentyear").innerHTML = currentYear;
 document.getElementById("lastModified").innerHTML = "Last Modification: " + document.lastModified;
 hamButton.addEventListener('click', () => {
     nav.classList.toggle('open');
     hamButton.classList.toggle('open');
+    para.classList.toggle('open');
+
 });
