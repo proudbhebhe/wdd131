8c0ccad2ba44af24d5b88aebd7b5bec4959e7b64
proudbhebhe <proudbhebhe02@gmail.com>
2026-07-20 12:46:55 +0200

    updated my code to a dd a few features

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,10 +2,24 @@ const today = new Date();
 const currentYear = today.getFullYear();
 
 function calculateWindChill(temp, windSpeed ){
-    return 35.74 + (0.6215 * temp) - (35.75 * windSpeed ** 0.16) + (0.4275 * temp * windSpeed ** 0.16);
+    return  35.74 + (0.6215 * temp) - (35.75 * windSpeed ** 0.16) + (0.4275 * temp * windSpeed ** 0.16);
 }
-const happy =calculateWindChill(30,15)
+
 document.getElementById("currentyear").innerHTML = currentYear;
 document.getElementById("lastModified").innerHTML = "Last Modification: " + document.lastModified;
 
-document.getElementById("chill").innerHTML =  happy.
+document.addEventListener("DOMContentLoaded", ()=> {
+    const temperatureText = document.getElementById("temperature").innerText;
+    const windText = document.getElementById("windSpeech").innerText;
+
+    const temperatureInt = parseFloat(temperatureText);
+    const windInt = parseFloat(windText);
+
+    windChill = "N/A";
+
+    if(temperatureInt<= 50 || windText> 3){
+    windChill = calculateWindChill(temperatureInt, windText);
+    }
+
+    document.getElementById("chill").innerText =  windChill + "℉";
+});
